5a13d2a3541a96cd6f829da264c4f76c60deddfe
Jurassic001 <mhaberer25@gmail.com>
2023-09-05 16:20:13 -0500

    operational multithreading yay!!!

--- auton(+manual).py
+++ auton(+manual).py
@@ -1,46 +1,33 @@
-# paste code from other .py files into this file to run them
+# Code by Max Haberer
 from djitellopy import tello
 import KeyPressModule as Gk
 import time
 import cv2
+from threading import Thread
 
 Gk.init()
 me = tello.Tello()
 me.connect()
 print(me.get_battery())
 has_launched = False
-
-global img
+keepRecording = True
 
 me.streamon()
+frame_read = me.get_frame_read()
 
 
-def movement(left_right, fwd_back, up_down, turn):
-    me.send_rc_control(left_right, fwd_back, up_down, turn)
+def videoRecorder():
+    while keepRecording:
+        img = me.get_frame_read().frame
+        img = cv2.resize(img, (360, 240))
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
 
 
-def halt():
-    while me.get_speed_x() or me.get_speed_y() or me.get_speed_z():
-        left_right, fwd_back, up_down, turn = 0, 0, 0, 0
-        me.send_rc_control(left_right, fwd_back, up_down, turn)
+recorder = Thread(target=videoRecorder)
+recorder.start()
 
 
-if not has_launched:
-    me.takeoff()
-    time.sleep(5)
-    has_launched = True
-
-
-while has_launched:
-    me.go_xyz_speed(100, -100, 0, 100)
-    img = me.get_frame_read().frame
-    img = cv2.resize(img, (450, 300))
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
-    if Gk.getkey("f"):
-        me.land()
-        me.streamoff()
-        time.sleep(3)
-        break
-    if Gk.getkey("c"):
-        cv2.imwrite(f'Resources/Images/{time.time()}.jpg', img)
+me.takeoff()
+me.go_xyz_speed(100, 0, 0, 20)
+me.land()
