e323f0630c9c268fce5130f90e0ff0a338a9d44d
Jurassic001 <mhaberer25@gmail.com>
2023-09-05 17:26:14 -0500

    refining manual override commands and making the viewing window bigger

--- auton(+manual).py
+++ auton(+manual).py
@@ -1,33 +1,63 @@
 # Code by Max Haberer
 from djitellopy import tello
-import KeyPressModule as Gk
 import time
 import cv2
 from threading import Thread
+import keyboard
 
-Gk.init()
 me = tello.Tello()
 me.connect()
 print(me.get_battery())
-has_launched = False
-keepRecording = True
 
 me.streamon()
 frame_read = me.get_frame_read()
 
 
-def videoRecorder():
-    while keepRecording:
+def videofeed():
+    while True:
         img = me.get_frame_read().frame
-        img = cv2.resize(img, (360, 240))
-        cv2.imshow("Image", img)
+        img = cv2.resize(img, (750, 500))
+        cv2.imshow("Live Feed", img)
         cv2.waitKey(1)
+        cv2.moveWindow("Live Feed", 0, 0)
+        if keyboard.is_pressed("f"):
+            me.land()
+            time.sleep(3)
+            cv2.destroyAllWindows()
+            me.streamoff()
+        if keyboard.is_pressed("space"):
+            me.emergency()
+            cv2.destroyAllWindows()
+            me.streamoff()
 
 
-recorder = Thread(target=videoRecorder)
-recorder.start()
+livestream = Thread(target=videofeed)
+livestream.start()
+
+
+def move(x, y, z):
+    me.go_xyz_speed(x, y, z, 100)
+
+
+def hover(wait):
+    me.send_rc_control(0, 0, 0, 0)
+    time.sleep(wait)
+
+
+def turn(degree):
+    me.rotate_clockwise(degree)
 
 
 me.takeoff()
-me.go_xyz_speed(100, 0, 0, 20)
+turn(45)
+move(500, 0, 50)
+move(100, 0, 0)
+hover(10)
+"""
+move(0, 50, 0)
+hover(1)
+move(0, 0, 50)
+hover(1)
+"""
 me.land()
+me.streamoff()
