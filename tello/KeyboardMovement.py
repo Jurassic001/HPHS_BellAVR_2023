2fe2d8467670defc2e81cab39c1a1ecf4888495f
Jurassic001 <mhaberer25@gmail.com>
2023-10-24 16:27:13 -0500

    manual control version 2

--- tello/KeyboardMovement.py
+++ tello/KeyboardMovement.py
@@ -7,34 +7,34 @@ me.connect()
 print(me.get_battery())
 
 
-def keyboard_control():
-    print("Press backspace to end the program")
-    while True:
-        lr, fb, ud, yv = 0, 0, 0, 0
-        speed = 500
-        if kb.is_pressed("a"):
-            lr = -speed
-        elif kb.is_pressed("d"):
-            lr = speed
-        if kb.is_pressed("w"):
-            fb = speed
-        elif kb.is_pressed("s"):
-            fb = -speed
-        if kb.is_pressed("r"):
-            ud = speed
-        elif kb.is_pressed("f"):
-            ud = -speed
-        if kb.is_pressed("q"):
-            yv = -speed
-        elif kb.is_pressed("e"):
-            yv = speed
-        if kb.is_pressed("x"):
-            me.land()
-            sleep(3)
-        if kb.is_pressed("z"):
-            me.takeoff()
-        me.send_rc_control(lr, fb, ud, yv)
+def keyboard_control(boolin):
+    if boolin:
+        print("Press backspace to end the program")
+        while True:
+            lr, fb, ud, yv = 0, 0, 0, 0
+            speed = 500
+            if kb.is_pressed("a"):
+                lr = -speed
+            elif kb.is_pressed("d"):
+                lr = speed
+            if kb.is_pressed("w"):
+                fb = speed
+            elif kb.is_pressed("s"):
+                fb = -speed
+            if kb.is_pressed("r"):
+                ud = speed
+            elif kb.is_pressed("f"):
+                ud = -speed
+            if kb.is_pressed("q"):
+                yv = -speed
+            elif kb.is_pressed("e"):
+                yv = speed
+            if kb.is_pressed("l"):
+                me.land()
+                sleep(3)
+            if kb.is_pressed("t"):
+                me.takeoff()
+            me.send_rc_control(lr, fb, ud, yv)
 
 
-while True:
-    keyboard_control()
+keyboard_control(True)
--- tello/main.py
+++ tello/main.py
@@ -1,7 +1,6 @@
 # Code by Max Haberer
 from customTello import CustomTello
 from threading import Thread
-from KeyboardMovement import keyboard_control
 import keyboard as kb
 import numpy as pymath
 import time
@@ -195,4 +194,28 @@ while auton:
     goHomeET("Landing pad")
     land("end")
 
-keyboard_control()
+while True:
+    lr, fb, ud, yv = 0, 0, 0, 0
+    speed = 500
+    if kb.is_pressed("a"):
+        lr = -speed
+    elif kb.is_pressed("d"):
+        lr = speed
+    if kb.is_pressed("w"):
+        fb = speed
+    elif kb.is_pressed("s"):
+        fb = -speed
+    if kb.is_pressed("r"):
+        ud = speed
+    elif kb.is_pressed("f"):
+        ud = -speed
+    if kb.is_pressed("q"):
+        yv = -speed
+    elif kb.is_pressed("e"):
+        yv = speed
+    if kb.is_pressed("l"):
+        me.land()
+        time.sleep(3)
+    if kb.is_pressed("t"):
+        me.takeoff()
+    me.send_rc_control(lr, fb, ud, yv)
