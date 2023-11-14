8c3ab8a8108599454c9bc70717c445cb1db5c5c0
Jurassic001 <mhaberer25@gmail.com>
2023-11-14 17:24:42 -0600

    changed the manual control code to use multithreading (very cool)

--- tello/KeyboardMovement.py
+++ tello/KeyboardMovement.py
@@ -1,4 +1,5 @@
 from customTello import CustomTello
+from threading import Thread
 import keyboard as kb
 import time
 import cv2
@@ -13,41 +14,42 @@ frame_read = me.get_frame_read()
 
 
 def keyboard_control():
-    lr, fb, ud, yv = 0, 0, 0, 0
-    speed = 500
-    if kb.is_pressed("a"):
-        lr = -speed
-    elif kb.is_pressed("d"):
-        lr = speed
-    if kb.is_pressed("w"):
-        fb = speed
-    elif kb.is_pressed("s"):
-        fb = -speed
-    if kb.is_pressed("r"):
-        ud = speed
-    elif kb.is_pressed("f"):
-        ud = -speed
-    if kb.is_pressed("q"):
-        yv = -speed
-    elif kb.is_pressed("e"):
-        yv = speed
-    if kb.is_pressed("l"):
-        me.land()
-        time.sleep(3)
-    if kb.is_pressed("t"):
-        me.takeoff()
-    if kb.is_pressed("backspace"):
-        me.emergency()
-    if kb.is_pressed("space"):
-        me.end()
-        exit()
-    if kb.is_pressed("down"):
-        me.cam("down")
-    elif kb.is_pressed("up"):
-        me.cam("fwd")
-    if kb.is_pressed("m"):
-        me.flip_back()
-    me.send_rc_control(lr, fb, ud, yv)
+    while True:
+        lr, fb, ud, yv = 0, 0, 0, 0
+        speed = 500
+        if kb.is_pressed("a"):
+            lr = -speed
+        elif kb.is_pressed("d"):
+            lr = speed
+        if kb.is_pressed("w"):
+            fb = speed
+        elif kb.is_pressed("s"):
+            fb = -speed
+        if kb.is_pressed("r"):
+            ud = speed
+        elif kb.is_pressed("f"):
+            ud = -speed
+        if kb.is_pressed("q"):
+            yv = -speed
+        elif kb.is_pressed("e"):
+            yv = speed
+        if kb.is_pressed("l"):
+            me.land()
+            time.sleep(3)
+        if kb.is_pressed("t"):
+            me.takeoff()
+        if kb.is_pressed("backspace"):
+            me.emergency()
+        if kb.is_pressed("space"):
+            me.end()
+            exit()
+        if kb.is_pressed("down"):
+            me.cam("down")
+        elif kb.is_pressed("up"):
+            me.cam("fwd")
+        if kb.is_pressed("m"):
+            me.flip_back()
+        me.send_rc_control(lr, fb, ud, yv)
 
 
 # noinspection PyUnresolvedReferences
@@ -60,4 +62,6 @@ def manual():
         keyboard_control()
 
 
-manual()
+livestream = Thread(target=manual)
+livestream.start()
+keyboard_control()
